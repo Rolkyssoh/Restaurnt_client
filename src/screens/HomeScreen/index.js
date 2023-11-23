7fb76eafc5250ce7fdc9c09968750ae68ddc5a94
Rolkyssoh <juventussoh@gmail.com>
2023-11-23 20:08:54 +0100

    start the filter by favorite

--- src/screens/HomeScreen/index.js
+++ src/screens/HomeScreen/index.js
@@ -2,36 +2,60 @@ import React, {useEffect, useState} from 'react';
 import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
 import RestaurantHome from './RestaurantHome';
 import ShopHome from './ShopHome';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, StyleSheet} from 'react-native';
 import {SearchBar} from '@rneui/themed';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 const TabTop = createMaterialTopTabNavigator();
 
 export const HomeScreen = ({navigation, route}) => {
   const [searchTerm, setSearchTerm] = useState('');
+  const [viewFavorites, setViewFavorites] = useState(false)
   useEffect(() => {
     console.log('the routes:', route);
     console.log('the navigation:', navigation);
   }, []);
 
+  const doHandleFavorite = (action) => {
+    console.log('Cliquedd!!', action)
+    setViewFavorites(!viewFavorites)
+  }
+
   return (
     <View style={{flex:1, backgroundColor:'#249689'}}>
-      <SearchBar
-        placeholder="Que recherchez vous?"
-        containerStyle={styles.searchBarContainer}
-        inputContainerStyle={{
-          height: 43,
-          backgroundColor: '#fff',
-          borderRadius: 20,
-        }}
-        inputStyle={{color: '#000'}}
-        placeholderTextColor="#000"
-        searchIcon={{color: '#000'}}
-        value={searchTerm}
-        onChangeText={e => setSearchTerm(e)}
-        onClear={() => setSearchTerm('')}
-      />
+      <View style={{flexDirection:'row', justifyContent:'center'}}>
+        <SearchBar
+          placeholder="Que recherchez vous?"
+          containerStyle={styles.searchBarContainer}
+          inputContainerStyle={{
+            height: 43,
+            backgroundColor: '#fff',
+            borderRadius: 20,
+          }}
+          inputStyle={{color: '#000'}}
+          placeholderTextColor="#000"
+          searchIcon={{color: '#000'}}
+          value={searchTerm}
+          onChangeText={e => setSearchTerm(e)}
+          onClear={() => setSearchTerm('')}
+        />
+        <View style={styles.singleView} />
+        <View style={styles.iconView}>
+          { viewFavorites ? <Ionicons
+            name="bookmark"
+            size={30}
+            color="#FF5963"
+            onPress={() => doHandleFavorite('remove')}
+          /> :
+          <Ionicons
+            name="bookmark-outline"
+            size={30}
+            color="#FF5963"
+            onPress={() => doHandleFavorite('add')}
+          />}
+        </View>
+      </View>
       <TabTop.Navigator
         screenOptions={{
           tabBarStyle:{ borderTopLeftRadius:40},
@@ -74,9 +98,34 @@ const styles = StyleSheet.create({
     borderRadius:25,
     backgroundColor: 'white',
     height: 45,
-    marginHorizontal: 10,
+    flex:1,
+    marginLeft: 10,
+    marginRight:5,
     marginVertical: 20,
     alignItems: 'center',
     justifyContent: 'center',
   },
+  singleView:{
+    backgroundColor:'#249689', 
+    // backgroundColor:'red', 
+    width:28, 
+    alignSelf:'center', 
+    height:45,
+    // marginLeft:-20,
+    borderTopLeftRadius:25,
+    borderBottomLeftRadius:25,
+    position:'absolute',
+    // left:324
+    right:60
+  },
+  iconView:{
+    backgroundColor:'#fff',
+    marginRight:10,
+    height:45,
+    width:45,
+    alignSelf:'center',
+    borderRadius:25,
+    justifyContent:'center',
+    alignItems:'center'
+  }
 });
